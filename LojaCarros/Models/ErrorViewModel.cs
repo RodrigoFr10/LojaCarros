b14d219d086236cc061b965b74b52deda4b4059b
85595195053 <85595195053@senacrs.edu.br>
2025-10-06 20:40:40 -0300

    primeiro commit

---
+++ LojaCarros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LojaCarros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
